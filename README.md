a96f9a6947affeee304d8c1fb98b44b2f13a8d2c
Tercio Lemos <tercio.lemos@gmail.com>
2024-12-30 23:53:27 -0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# kafka-producer-consumer-csharp
+# .NET 8 Kafka - Producer & consumer
+
+In this example, you will build C# client applications which produce and consume messages from an Apache Kafka® cluster.
+
+This example assumes that you already have .NET Core (>= 8.0) installed.
+
+## Create a Project
+
+```
+mkdir kafka-dotnet-getting-started 
+cd kafka-dotnet-getting-started
+mkdir producer
+mkdir consumer
+```
---
+++ consumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    // User-specific properties that you must set
+    BootstrapServers = "<BOOTSTRAP SERVERS>",
+    SaslUsername = "<CLUSTER API KEY>",
+    SaslPassword = "<CLUSTER API SECRET>",
+
+    // Fixed properties
+    SecurityProtocol = SecurityProtocol.SaslSsl,
+    SaslMechanism = SaslMechanism.Plain,
+    GroupId = "kafka-dotnet-getting-started",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+const string topic = "purchases";
+
+CancellationTokenSource cts = new CancellationTokenSource();
+Console.CancelKeyPress += (_, e) => {
+    e.Cancel = true; // prevent the process from terminating.
+    cts.Cancel();
+};
+
+using (var consumer = new ConsumerBuilder<string, string>(config).Build())
+{
+    consumer.Subscribe(topic);
+    try
+    {
+        while (true)
+        {
+            var cr = consumer.Consume(cts.Token);
+            Console.WriteLine($"Consumed event from topic {topic}: key = {cr.Message.Key,-10} value = {cr.Message.Value}");
+        }
+    }
+    catch (OperationCanceledException)
+    {
+        // Ctrl-C was pressed.
+    }
+    finally
+    {
+        consumer.Close();
+    }
+}
